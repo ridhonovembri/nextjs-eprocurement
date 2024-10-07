6e382280e7b6c389ecf077a4eddc39a68b8bac61
Novembri <Ridho.Novembri@mcdermott.com>
2024-10-07 21:11:46 +0700

    change picture slider

--- src/components/home/slider/slider.jsx
+++ src/components/home/slider/slider.jsx
@@ -1,11 +1,9 @@
 "use client";
 import React, { useCallback } from "react";
 import styles from "./slider.module.css";
-import Slide1 from "/public/bg03.jpg";
-import Slide2 from "/public/bg06.jpg";
-import Slide3 from "/public/bg12.jpg";
-import Slide4 from "/public/bg16.jpg";
-import Slide5 from "/public/bg21.jpg";
+import Slide1 from "/public/batamindo-depan.jpg";
+import Slide2 from "/public/batamindo-dormitory.jpg";
+import Slide3 from "/public/batamindo-pabrik.jpg";
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
@@ -25,19 +23,13 @@ const Slider = () => {
     <div className={styles.emba} ref={emblaRef}>
       <div className={styles.embla__container}>
         <div className={styles.embla__slide}>
-          <Image src={Slide3} className={styles.img} />
+          <Image src={Slide1} className={styles.img} />
         </div>
         <div className={styles.embla__slide}>
           <Image src={Slide2} className={styles.img} />
         </div>
         <div className={styles.embla__slide}>
-          <Image src={Slide4} className={styles.img} />
-        </div>
-        <div className={styles.embla__slide}>
-          <Image src={Slide5} className={styles.img} />
-        </div>
-        <div className={styles.embla__slide}>
-          <Image src={Slide1} className={styles.img} />
+          <Image src={Slide3} className={styles.img} />
         </div>
         {/* <button onClick={scrollNext} enabled={prevb}>Next</button>
         <button onClick={scrollPrev} className={styles.button}>Prev</button> */}
